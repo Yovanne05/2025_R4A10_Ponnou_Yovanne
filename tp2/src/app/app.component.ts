a2d23dd766d1cb75aec65670f13a5c1d59b95900
Yovanne <yovanne.ponnou@gmail.com>
2025-02-08 12:20:47 +0100

    decomposition des composants

--- tp2/src/app/app.component.ts
+++ tp2/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponents } from "./components/header/header.component";
 import { VirtualHealthcareComponents } from "./components/virtual_healthcare/virtual_healthcare.component";
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponents, VirtualHealthcareComponents],
+  imports: [RouterOutlet, HeaderComponent, VirtualHealthcareComponents],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- tp2/src/app/components/header/header.component.ts
+++ tp2/src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from "@angular/core";
+import { TitleComponent } from "./title/title.component";
+import { NavComponent } from "./nav/nav.component";
 
 @Component({
   selector: 'header-root',
   standalone: true,
+  imports : [TitleComponent, NavComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
 
-export class HeaderComponents{
+export class HeaderComponent{
   
 }
