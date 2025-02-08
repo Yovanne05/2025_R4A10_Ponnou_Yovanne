aa5f2ea415d15dba71485c97921a812a687d19a0
Yovanne <yovanne.ponnou@gmail.com>
2025-02-08 20:18:03 +0100

    ajout de  la description pr les services

---
+++ tp2/src/app/components/our_services/description/description.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'description-root',
+  standalone: true,
+  templateUrl: './description.component.html',
+  styleUrl: './description.component.sass'
+})
+
+export class DescriptionComponent {
+    
+}
--- tp2/src/app/components/our_services/our_services.component.ts
+++ tp2/src/app/components/our_services/our_services.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import {TitleComponent} from "../title/title.component";
 import { AllCardComponent } from "./allCard/allCard.component";
 import { LearnMoreComponent } from "./learn_more/learn_more.component";
+import { DescriptionComponent } from "./description/description.component";
 
 @Component({
   selector: 'our-services-root',
   standalone: true,
-  imports: [TitleComponent, AllCardComponent, LearnMoreComponent],
+  imports: [TitleComponent, AllCardComponent, LearnMoreComponent, DescriptionComponent],
   templateUrl: './our_services.component.html',
   styleUrl: './our_services.component.sass'
 })
