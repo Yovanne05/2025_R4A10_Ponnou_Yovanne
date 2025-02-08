c97615fcf6689084959060088e201354c575a03a
Yovanne <yovanne.ponnou@gmail.com>
2025-02-08 16:36:10 +0100

    ajout boutton learn more responsive

---
+++ tp2/src/app/components/our_services/learn_more/learn_more.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'lear-more-root',
+  standalone: true,
+  templateUrl: './learn_more.component.html',
+  styleUrl: './learn_more.component.sass'
+})
+
+export class LearnMoreComponent {
+    
+}
--- tp2/src/app/components/our_services/our_services.component.ts
+++ tp2/src/app/components/our_services/our_services.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import {TitleComponent} from "./title/title.component";
 import { AllCardComponent } from "./allCard/allCard.component";
+import { LearnMoreComponent } from "./learn_more/learn_more.component";
 
 @Component({
   selector: 'our-services-root',
   standalone: true,
-  imports : [TitleComponent, AllCardComponent],
+  imports: [TitleComponent, AllCardComponent, LearnMoreComponent],
   templateUrl: './our_services.component.html',
   styleUrl: './our_services.component.sass'
 })
