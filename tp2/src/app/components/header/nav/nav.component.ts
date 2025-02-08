d5e32aa43457fdbea3655aea5e48a534662414ed
Yovanne <yovanne.ponnou@gmail.com>
2025-02-08 12:34:15 +0100

    Composant du header rendu dynamique

--- tp2/src/app/components/header/nav/nav.component.ts
+++ tp2/src/app/components/header/nav/nav.component.ts
@@ -1,12 +1,18 @@
 import { Component } from "@angular/core";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'nav-root',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.sass'
 })
-
 export class NavComponent {
-  
+  links = [
+    { nom: "Home", url: "#" },
+    { nom: "Find a doctor", url: "#" },
+    { nom: "Apps", url: "#" },
+    { nom: "Testimonials", url: "#" }
+  ];
 }
--- tp2/src/app/components/header/title/title.component.ts
+++ tp2/src/app/components/header/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'title-root',
@@ -8,5 +8,5 @@ import { Component } from "@angular/core";
 })
 
 export class TitleComponent {
-  
+  @Input() title: string = "Trafalgar";
 }
