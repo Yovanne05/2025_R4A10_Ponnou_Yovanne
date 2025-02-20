de78a2a0dafce484028407b7809c7cfdffde2bba
Yovanne <yovanne.ponnou@gmail.com>
2025-02-20 19:34:00 +0100

    fix css (manque pagination)

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  nom: string = 'Voyages';
+  currentYear: number = new Date().getFullYear();
 }
